2f54b40e1cf621cd9a58845e9103cc9b0b29a823
Mattair39 <105869714+Mattair39@users.noreply.github.com>
2024-05-02 07:26:10 -0500

    Add project files.

---
+++ Models/Carrera.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenProgreso1.Models
+{
+    public class Carrera
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string nombre_Carrera { get; set; }
+
+        public string campus {  get; set; }
+
+        [Required]
+        public int numero_semestres { get; set; }
+
+
+    }
+}
---
+++ Models/MArguello.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ExamenProgreso1.Models
+{
+    public class MArguello
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime fechaNacimiento { get; set; }
+
+        [AllowNull]
+
+        public decimal peso { get; set; }
+
+        [Required]
+        public Boolean esEcuatoriano { get; set; }
+
+        [ForeignKey("CarreraId")]
+
+        [AllowNull]
+        public int CarreraId { get; set; }
+
+        public Carrera Carrera { get; set; }
+       
+
+
+    }
+}
